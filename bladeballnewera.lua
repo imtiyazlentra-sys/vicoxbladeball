d09688c43254f6b935c13d0368d5b3c423a1f457
imtiyazlentra-sys <imtiyazlentra@gemanurani.sch.id>
2025-11-30 13:15:03 +0700

    Update bladeballnewera.lua

--- bladeballnewera.lua
+++ bladeballnewera.lua
@@ -99,8 +99,6 @@ local lastTrailUpdateTime = 0
 
 local BallBillboard = nil
 local PlayerBillboard = nil
-getgenv().FirstParryDone = false
-local FirstParryDone = getgenv().FirstParryDone
 local lastUpdateTime = 0
 
 local ballBillboardEnabled = false
@@ -1102,53 +1100,50 @@ function Auto_Parry.Parry(Parry_Type)
     if tick() - lastParryTime < parryCooldown then 
         return false 
     end
+
+     local ball_properties = AutoParry.ball.properties
+        
     lastParryTime = tick()
-    local presses = getgenv().speeddo or 1
-    local parryData = Auto_Parry.Parry_Data(Parry_Type)
-    
-    if not FirstParryDone then
-        VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
-        task.wait(0.015)
-        VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 0)
-        getgenv().FirstParryDone = true
-        FirstParryDone = true
-        Notification.new("success", "Parry", "Parry Pressed!", true, 2)
-        return
-    end
-   
-    for i = 1, presses do
-        if not next(ParryRemotes) then
-            warn("[Parry] Remote belum tertangkap!")
-            return
+
+    local presses = getgenv().speeddo and isSpam or 1
+
+    if activeMethod == "F Key" then
+        for i = 1, presses do
+            VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+            VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 0)
         end
-         for remote, originalArgs in pairs(ParryRemotes) do
-                    local modifiedArgs = {
-                        originalArgs[1],
-                        originalArgs[2],
-                        originalArgs[3],
-                        parryData[2],
-                        parryData[3],
-                        parryData[4],
-                        originalArgs[7]
-                    }
-                    if remote:IsA("RemoteEvent") then
-                        remote:FireServer(unpack(modifiedArgs))
-                    elseif remote:IsA("RemoteFunction") then
-                        remote:InvokeServer(unpack(modifiedArgs))
-            end
-        end
+    else
+        local Parry_Data = Auto_Parry.Parry_Data(Parry_Type)
+        for remote, originalArgs in pairs(ParryRemotes) do
+        local modifiedArgs = {originalArgs[1], originalArgs[2], 0, Parry_Data[2], Parry_Data[3], Parry_Data[4]}
+        if remote:IsA("RemoteEvent") then
+            remote:FireServer(unpack(modifiedArgs))
+        elseif remote:IsA("RemoteFunction") then
+            remote:InvokeServer(unpack(modifiedArgs))
+                 end
+             end   
     end
-    
+
+    if Parries > 7 then
+        return false
+    end
+
     Parries += 1
-    AutoParry.ball.properties.parries += 1
+
+    ball_properties.parries += 1
+
     task.delay(0.35, function()
-        if Parries > 0 then Parries -= 1 end
-        if AutoParry.ball.properties.parries > 0 then
-            AutoParry.ball.properties.parries -= 1
+        if Parries > 0 then
+            Parries -= 1
+        end
+    end)
+     task.delay(0.35, function()
+        if ball_properties.parries > 0 then
+            ball_properties.parries -= 1
+            AutoParry.ball.properties.parries = ball_properties.parries
         end
     end)
 end
-
 
 local Lerp_Radians = 0
 local Last_Warping = tick()
@@ -1420,8 +1415,10 @@ Auto_Parry.Spam_Service = function()
     local compensation = 0 + math.pi
 	
     local Maximum_Spam_Distance = PingAdjustment + math.min(speed / 4, 200) + compensation
-    if not self.entityProps or not self.ballProps then return 0 end
-    if entityProps.Distance > Maximum_Spam_Distance or self.ballProps.Distance > Maximum_Spam_Distance or LocalPlayer:DistanceFromCharacter(closest.PrimaryPart.Position) > Maximum_Spam_Distance then return 0 end
+    local entityProps = Auto_Parry.GetEntityProps()
+    local ballProps = Auto_Parry.GetBallProps()
+    if not entityProps or not ballProps then return 0 end
+    if entityProps.Distance > Maximum_Spam_Distance or ballProps.Distance > Maximum_Spam_Distance or LocalPlayer:DistanceFromCharacter(closest.PrimaryPart.Position) > Maximum_Spam_Distance then return 0 end
     local Maximum_Speed = 5 - math.min(speed / 5, 5)
     local Maximum_Dot = math.clamp(dot, -1, 0) * Maximum_Speed
     local Spam_Accuracy = Maximum_Spam_Distance - Maximum_Dot
@@ -2576,10 +2573,7 @@ local SpamParry = rage:create_module({
                 local entityProps = Auto_Parry.GetEntityProps()
 
                 
-                local Spam_Accuracy = Auto_Parry.Spam_Service({
-                    ballProps =  ballProps,
-                    entityProps = entityProps
-                })
+                local Spam_Accuracy = Auto_Parry.Spam_Service()
 
                 if Spam_Accuracy <= 0 then
                     return
@@ -6094,6 +6088,9 @@ workspace.Balls.ChildRemoved:Connect(function(Value)
 end)
 
 
+
+
 main:load()  
+
 
 
